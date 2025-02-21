af528eb55cc59aacfd608828cc31479dd5f95971
AradD7 <arad.delaramnikoo@torontomu.ca>
2025-02-21 16:19:48 -0500

    player movement is functional

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ def main():
     pygame.init()
     time = pygame.time.Clock()
     dt = 0
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
+        updatable_group.update(dt)
+        for drawing in drawable_group:
+            drawing.draw(screen)
         pygame.display.flip()
         dt = time.tick(60) / 1000
 
